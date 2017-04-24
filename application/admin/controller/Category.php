8ee73198948431685f6ec53a978a8feff587e3ec
zmore7 <guoqingzhe@hotmail.com>
2017-04-24 15:36:36 +0800

    正在写分类删除

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -23,15 +23,38 @@ class Category extends Base
         $this -> assign('catelist',$data);
         return $this -> fetch('cate_list');
     }
-    public function add_cate(){
+    public function cate_add(){
         $Cate = new Cate();
-        if(input('post')){
-            var_dump(input('post.'));
+        if(input('post.')){
+            //var_dump(input('post.'));
+            $data = input('post.');
+            //var_dump($data);
+            $rst = $Cate -> addCate($data);
+            if($rst){
+                return $this->success('增加分类成功','Category/cate_list');
+            }else{
+                return $this->error('增加分类失败');
+            }
         }else{
             $data = $Cate -> getCateList();
             $this -> view -> engine->layout('layout/admin_layout');
             $this -> assign('catelist',$data);
-            return $this -> fetch('add_cate');
+            return $this -> fetch('cate_add');
+        }
+    }
+    public function cate_del(){
+        if(input('get.')) {
+            $Cate = new Cate();
+            $rst = $Cate->delCate(input('get.cat_id'));
+            if ($rst == 0) {
+                return $this->error('分类不存在');
+            } else {
+                if($rst){
+                    return $this->success('删除分类成功');
+                }else{
+                    return $this->error('删除分类失败');
+                }
+            }
         }
     }
 }
--- application/admin/model/Cate.php
+++ application/admin/model/Cate.php
@@ -18,4 +18,39 @@ class Cate extends Model
             ->select();
         return $db;
     }
+    public function addCate($data){
+        $data['created_time'] = date("Y:m:d H:i:s");
+        if($data['parent_id']==9999){
+            $data['cat_level']=0;
+        }else{
+            //目前只做到二级分类（0、1、2级）
+            $level = Db::name('category')
+                ->where('cat_id','=',$data['parent_id'])
+                ->select();
+            if($level[0]['cat_level']==0){
+                $data['cat_level'] = 1;
+            }else{
+                $data['cat_level'] = 2;
+            }
+        }
+        $db = Db::name('category')
+            ->insert($data);
+        return $db;
+    }
+    public function delCate($id){//注意连带删除！
+        $ext = Db::name('category')
+            ->where('cat_id','=',$id)
+            ->select();
+        if($ext==NULL){
+            return 0;
+        }else{
+            $db = Db::name('category')//本分类下的子分类的删除
+                ->where('parent_id','=',$id)
+                ->delete();
+            $db = Db::name('category')//本分类下的子分类的删除
+                ->where('cat_id','=',$id)
+                ->delete();
+        }
+        return $db;
+    }
 }
