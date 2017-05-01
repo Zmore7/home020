a330bfb5ff2e185068b596356becef761f85f3a1
zmore7 <guoqingzhe@hotmail.com>
2017-05-01 20:02:30 +0800

    经纪人差增加雇员功能

--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -148,5 +148,99 @@ class User extends Model
             ->insert($data);
         return $db;
     }
+    //经纪人操作
+    public function getMiddlemanList(){
+        $db = Db::name('middleman')
+            ->select();
+        return $db;
+    }
+    public function getMiddlemanDetail($id){
+        $db = Db::name('middleman')
+            ->where('id','=',$id)
+            ->select();
+        $category_arr = explode(',',$db[0]['category']);
+        //var_dump($category_arr);
+        $arr_num = count($category_arr);
+        $category_str_arr = array();
+        for($i = 0;$i < $arr_num;$i++){
+            $category_str_arr[$i] = Db::name('category')->where('cat_id','=',$category_arr[$i])->select();
+        }
+        //var_dump($category_str_arr);
+        $db['category_str_arr'] = $category_str_arr;
+        //var_dump($db);
+        return $db;
+    }
+    public function getMiddlemanDetail_Mid($id){
+        $db = Db::name('middleman')
+            ->where('id','=',$id)
+            ->select();
+        $category_arr = explode(',',$db[0]['category']);
+        //var_dump($category_arr);
+        $arr_num = count($category_arr);
+        $category_str_arr = array();
+        $category_str_arr = Db::name('category')
+            ->select();
+        //var_dump($category_str_arr);
+        $db['category_str_arr'] = $category_str_arr;
+        //var_dump($db);
+        return $db;
+    }
+    public function getMiddlemanEmployee($mid){
+        $db = Db::name('aunt')
+            ->where('mid','=',$mid)
+            ->select();
+        $employee_num = count($db);
+        $category_arr_str = "";
+        for($i = 0;$i < $employee_num;$i++){
+            $category_arr = explode(',',$db[$i]['purpose']);
+            $purpose_num = count($category_arr);
+            for($j = 0;$j < $purpose_num;$j++){
+                $temp = Db::name('category')->field('cat_name')->where('cat_id','=',$category_arr[$j])->select();
+                //var_dump($temp);
+                $category_arr[$j] = $temp;
+                //var_dump($category_arr);
+
+            }
+            $db[$i]['cat_name_arr'] = $category_arr;
+            //var_dump($db[$i]['cat_name_arr']);
+        }
+
+        return $db;
+    }
+    public function delMiddleman($id){
+        $db = Db::name('middleman')
+            ->where('id','=',$id)
+            ->select();
+        if($db==NULL){
+            return 0;
+        }else{
+            $db = Db::name('middleman')
+                ->where('id','=',$id)
+                ->delete();
+            return $db;
+        }
+    }
+    public function editMiddleman($id,$data){
+        $data['category'] = implode(',',$data['category']);
+        $db = Db::name('middleman')
+            ->where('id','=',$id)
+            ->select();
+        if($db==NULL){
+            return 0;
+        }else{
+            $db = Db::name('middleman')
+                ->where('id','=',$id)
+                ->update($data);
+            return $db;
+        }
+    }
+    public function addMiddleman($data){
+        $data['category'] = implode(',',$data['category']);
+        $data['password'] = md5($data['password']);
+        $db = Db::name('middleman')
+            ->insert($data);
+        return $db;
+    }
+
 
 }
